2043162badcb0d04cfaac7e33e51b89be64fcd1b
Ronan <ronan@ronanfuruta.com>
2023-02-18 15:24:41 -0500

    Initial Commit

---
+++ SpamSucker/SpamSuckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpamSuckerApp.swift
+//  SpamSucker
+//
+//  Created by Ronan Furuta on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct SpamSuckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
